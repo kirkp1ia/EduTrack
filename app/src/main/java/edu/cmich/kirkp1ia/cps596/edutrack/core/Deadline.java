013a6661b381c983ee9f663a2c54ca1cd23bafff
Kirkpatrick <kirkp1ia@cps145350pe400.cps.cmich.edu>
2016-11-01 16:35:59 -0400

    Documentation

---
+++ app/src/main/java/edu/cmich/kirkp1ia/cps596/edutrack/core/Deadline.java
@@ -0,0 +1,17 @@
+package edu.cmich.kirkp1ia.cps596.edutrack.core;
+
+import java.util.Calendar;
+
+/**
+ * Created by kirkp1ia on 11/1/16.
+ */
+public class Deadline {
+
+    private String description;
+    private String notes;
+    private Calendar deadline;
+
+    public Deadline(String date) {
+
+    }
+}
